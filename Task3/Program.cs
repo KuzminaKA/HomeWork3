2e20c75a087f78369c9a69c1184ba99f6763c77d
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-03-31 11:46:04 +0300

    Third task commit

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+System.Console.Write("Введите число: ");
+int N= int.Parse(Console.ReadLine());
+
+void Cube(int N)
+{
+for(int n=1; n<=N; n++)
+
+    {
+      int result= n*n*n;
+      System.Console.WriteLine(result);
+    }
+
+  }
+
+Cube(N);
